9de1efab0e7c132241db261f0022ef964312cd02
LiXizhi <LiXizhi>
2016-09-13 00:53:42 +0800

    config file

--- start.bat
+++ start.bat
@@ -1,8 +1,8 @@
-rm log.txt
+rem rm log.txt
 
 rem update package dependencies
 git submodule init && git submodule update
 git submodule foreach git pull origin master
 
 rem start webserver in folder and port
-npl -d root="WebServerExample/" port="8099" bootstrapper="script/apps/WebServer/WebServer.lua" 
+npl -d root="WebServerExample/" port="8099" bootstrapper="script/apps/WebServer/WebServer.lua" servermode="true"
